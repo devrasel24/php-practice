aeff6f34ee7df18f14d12ee81d87eeb06478da7d
devrasel24 <developerrasel24@gmail.com>
2023-07-31 18:11:09 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,114 @@
+<?php
+
+// $productPrices = [10, 20, 50, 10, 20, 40, 50];
+
+// $totalPrice = 0;
+
+// foreach ($productPrices as $myPrice){
+
+//     $totalPrice *= $myPrice;
+// }
+
+
+// echo $totalPrice;
+
+// echo <br>;
+
+//If else
+
+
+// if( $age >= 18){
+
+//     echo "Your are boss";
+
+// }else{
+//     echo "You are tinny";
+// }
+
+// $age = 25;
+
+
+
+// $x = "rrrr";
+
+// var_dump(is_int($x));
+
+// echo (max(20, 30, 50));
+
+// echo <br>;
+
+// define("name","rasel khan");
+// echo name;
+
+// define("food", [
+//     "alu", "potol", "Begun"
+// ]);
+
+// echo food[0];
+
+
+// $age = 1;
+
+// if( $age>= 18 ){
+//     echo "You are eligibe for youtube and facebook";
+// }elseif($age <= 18 & $age >= 13 ){
+//     echo "You are eligible for only youtube";
+// }else{
+//     echo "Not eligible for anythink";
+// }
+// --------------------------------------------------------
+// $car = "breaks";
+
+// switch ($car) {
+    
+//     case "start";
+//         echo "This car is starting";
+//         break;
+
+//     case "stop";
+//         echo "This car is Stopped";
+//         break;
+//     case "breaks";
+//         echo "This car breaks";
+//         break;
+//     default:
+//         echo "Please Use any buttons";
+// }
+
+
+// ------------------------- Class 04 --------------------------------------
+
+
+// $a = 5;
+// ++$a;
+// echo "First Number " . ++$a . " - - " . $a;
+
+// for loop
+
+// for ($i = 100; $i >= 1; $i--){
+//     echo "This is loop <br>";
+// }
+
+// while loop
+
+// $i = 0;
+
+// while ($i <= 10){
+
+//      echo "This nothibg <br>";
+//      $i++;
+// }
+
+// do while
+
+
+// $i = 1;
+// do{
+//     echo "This nothibg <br>";
+//     $i++;
+// }while($i <=10);
+
+for ($i = 1991; $i <= 2022; $i++ ){
+    echo $i;
+    echo "<br>";
+}
